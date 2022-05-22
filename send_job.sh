cbb992cabd64e48c95e5d7965c2dbb7f6c76a93b
Robert Benn <mnk884@rescomp1.hpc.in.bmrc.ox.ac.uk>
2022-05-22 19:07:39 +0100

    configure job sending

--- send_job.sh
+++ send_job.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+module load Python/3.8.6-GCCcore-10.2.0
+source /well/margulies/users/mnk884/python/pkReliability-skylake/bin/activate
+
+### job parameters
+
+#$ -P PkRel
 #$ -cwd
 #$ -q short.qc
 #$ -j y
 #$ -o ./logs
-
+#$ -pe shmem 9
 
 SUBJECT_LIST=./subjectList.txt
 nsubjs=`wc -l < $SUBJECT_LIST`
 
+### each subject forms one job of the array job
+
 #$ -t 1-$nsubjs
 
 
-
+####get file name 
 FILENAME=$(sed -n "${SGE_TASK_ID}p" $SUBJECT_LIST)
 
 echo "Processing subject $FILENAME"
 
 # Load a recent python module
-module load Python/3.7.0-foss-2018b
 
 # run pk reliability for subject 
-python pkReliability.py --subj $FILENAME  100206 --odir /well/margulies/projects/pkReliability 
+python pkReliability.py --subj $FILENAME  --odir /well/margulies/projects/pkReliability 
