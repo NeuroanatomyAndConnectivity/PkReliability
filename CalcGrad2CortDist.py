ba07be75c146775949903095ae5d7ea8ea974dbf
austin benn <austin@austins-MacBook-Air-2.local>
2022-10-08 14:11:59 +0200

    fixed indentation

--- CalcGrad2CortDist.py
+++ CalcGrad2CortDist.py
@@ -9,12 +9,10 @@ subj=sys.argv[1]
 print(f'calculating distance from gradient mask to  parcels for subject {subj}')
 
 def DistFromGradMask(subj,threshold):
-
 	subj_inst=hcp_subj(subj)
-
 	grads=subj_inst.extract_topX(subj_inst.Lgrad,subj_inst.Rgrad,threshold)
-    Lsurf=[subj_inst.Lcoords,subj_inst.Lfaces]
-    Ldist=surfdist.analysis.dist_calc(Lsurf,subj_inst.Lfill,grads[0])
+	Lsurf=[subj_inst.Lcoords,subj_inst.Lfaces]
+	Ldist=surfdist.analysis.dist_calc(Lsurf,subj_inst.Lfill,grads[0])
     
     Rsurf=[subj_inst.Rcoords,subj_inst.Rfaces]
     Rdist=surfdist.analysis.dist_calc(Rsurf,subj_inst.Rfill,grads[1])
@@ -25,11 +23,10 @@ def DistFromGradMask(subj,threshold):
     Larea=np.sqrt(np.sum(nib.load('L.area.func.gii').darrays[0].data[subj_inst.Lfill]))
     sp.run('rm L.area.func.gii',shell=True)
     
-    
     sp.run(f'wb_command -surface-vertex-areas {subj_inst.Rsrf} R.area.func.gii',shell=True)
     Rarea=np.sqrt(np.sum(nib.load('R.area.func.gii').darrays[0].data[subj_inst.Rfill]))
     sp.run('rm R.area.func.gii',shell=True)
-     
+
     return [Ldist,Larea,Rdist,Rarea]
 
 results=DistFromGradMask(subj,90)
