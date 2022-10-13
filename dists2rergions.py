05acc0cdee66906aabaefffb30d099aa42773d53
Austin Benn <r.austinbenn@gmail.com>
2022-10-13 17:17:25 +0200

    create dist2regions
    calculates mean distance across individuals using masks derived from 70% probability peaks in the angular gyrus and lateral superior frontal peak to the deepest point of S1.

---
+++ dists2rergions.py
@@ -0,0 +1,36 @@
+from hcp_class import hcp_subj
+import surfdist.analysis
+import nibabel as nib
+from nilearn.plotting import view_surf
+import numpy as np
+
+subj=sys.argv[1]
+print(f'calculating distance from gradient mask to  parcels for subject {subj}')
+
+### load the defined ROIs
+Lfroi=np.load('2DistRois/LfRoi.npy')
+Lproi=np.load('2DistRois/LpRoi.npy')
+Rfroi=np.load('2DistRois/RfRoi.npy')
+Rproi=np.load('2DistRois/RpRoi.npy')
+
+def deepS1(subjClass):
+    Lsulc=nib.load(subjClass.Lsulc).darrays[0].data
+    Ltarg=subjClass.LS1[np.argmin(Lsulc[subjClass.LS1])]
+    
+    Rsulc=nib.load(subjClass.Rsulc).darrays[0].data
+    Rtarg=subjClass.RS1[np.argmin(Rsulc[subjClass.RS1])]
+    return Ltarg,Rtarg
+  
+inst=hcp_subj(subj,4)
+targets=deepS1(inst)
+Lsurf=([inst.Lcoords,inst.Lfaces])
+Ldist=surfdist.analysis.dist_calc(Lsurf,inst.Lfill,targets[0])
+Lfront=Ldist[LfROI]
+Lpost=Ldist[LpROI]
+
+Rsurf=([inst.Rcoords,inst.Rfaces])
+Rdist=surfdist.analysis.dist_calc(Rsurf,inst.Rfill,targets[1])
+Rfront=Rdist[RfROI]
+Rpost=Rdist[RpROI]
+
+np.save(f'2DistRois/{subj}.npy',np.asarray([Lfront.mean(),Lpost.mean(),Rfront.mean(),Rpost.mean()])
