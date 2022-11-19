e2467135112a3d4b52ee22bfa3c747d0aa4d6301
Austin Benn <r.austinbenn@gmail.com>
2022-11-19 13:01:34 +0100

    Update peaks2cortex.py

--- peaks2cortex.py
+++ peaks2cortex.py
@@ -14,6 +14,7 @@ import surfdist
 
 
 subj=sys.argv[1]
+print(f'running subject {subj}')
 out=f'Dist2SensoryBorder/{subj}/'
 os.makedirs(out,exist_ok=True)
 os.makedirs(f'{out}/spinBatches',exist_ok=True)
