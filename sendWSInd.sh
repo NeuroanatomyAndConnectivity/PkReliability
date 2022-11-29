569821f10d633668a30048dd4f6362a074a8fd41
Austin Benn <r.austinbenn@gmail.com>
2022-11-29 13:11:44 +0100

    switch to slurm - max 10 at a time

--- sendWSInd.sh
+++ sendWSInd.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
+### job parameters
+#SBATCH --job-name=Watershed
+#SBATCH -o ./logs/Watershed-%j.out
+#SBATCH -p short
+#SBATCH --constraint="skl-compat"
+#SBATCH --cpus-per-task=8
+#SBATCH --array=1-912%10
+#SBATCH --requeue
+SUBJECT_LIST=./subjectsWithParietalPeak.txt
+
 
 module load ConnectomeWorkbench/1.4.2-rh_linux64
 module load Python/3.9.6-GCCcore-11.2.0
 source /well/margulies/users/mnk884/python/postHoc-permutations-skl/bin/activate
 
+echo Executing task ${SLURM_ARRAY_TASK_ID} of job ${SLURM_ARRAY_JOB_ID} on `hostname` as user ${USER} 
 
-### job parameters
-#$ -N Watershed
-#$ -o logs/
-#$ -e logs/
-#$ -j y
-#$ -cwd
-#$ -q short.qc
-#$ -pe shmem 3
-#$ -t 1-912
+echo the job id is $SLURM_ARRAY_JOB_ID
+FILENAME=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $SUBJECT_LIST)
+echo echo $SLURM_ARRAY_JOB_ID
+echo "Processing subject $FILENAME"
 
-
-SUBJECT_LIST=./subjectsWithParietalPeak.txt
 
 SUBJECT=$(sed -n "${SGE_TASK_ID}p" $SUBJECT_LIST)
-echo python3 peaks2cortex.py  $SUBJECT $i
+echo python3 -u IndividualWatershed.py $SUBJECT
 python3 -u IndividualWatershed.py $SUBJECT
