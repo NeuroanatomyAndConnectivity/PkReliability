2de16183105c0147415796f5bf759d2b4673a082
Austin Benn <r.austinbenn@gmail.com>
2022-11-29 13:18:23 +0100

    pass subject

--- sendWSInd.sh
+++ sendWSInd.sh
@@ -15,13 +15,19 @@ module load Python/3.9.6-GCCcore-11.2.0
 source /well/margulies/users/mnk884/python/postHoc-permutations-skl/bin/activate
 
 echo Executing task ${SLURM_ARRAY_TASK_ID} of job ${SLURM_ARRAY_JOB_ID} on `hostname` as user ${USER} 
+### each subject forms one job of the array job
 
+echo "smoothing kernel is" ${smooth_kernel}
+
+####get file name 
 echo the job id is $SLURM_ARRAY_JOB_ID
 FILENAME=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $SUBJECT_LIST)
 echo echo $SLURM_ARRAY_JOB_ID
 echo "Processing subject $FILENAME"
 
+# Load a recent python module
+
 
 SUBJECT=$(sed -n "${SGE_TASK_ID}p" $SUBJECT_LIST)
 echo python3 -u IndividualWatershed.py $SUBJECT
-python3 -u IndividualWatershed.py $SUBJECT
+python3 -u IndividualWatershed.py $FILENAME
