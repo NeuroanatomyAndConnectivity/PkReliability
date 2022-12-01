78695d43a6e7894a9d3cad1414d4dfb1d632f8c3
Austin Benn <r.austinbenn@gmail.com>
2022-12-01 18:20:23 +0100

    Create IndiThr75to99Dists.py

---
+++ IndiThr75to99Dists.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+from hcp_class import hcp_subj
+import numpy as np
+import gdist
+import surfdist.analysis
+import surfdist.utils
+import surfdist
+import nibabel as nib
+import pickle
+import sys
+import os
+
+subj=sys.argv[1]
+out=f'/well/margulies/projects/pkReliability/Dist2SensoryBorder/{subj}/'
+os.makedirs(out,exist_ok=True)
+
+subj=hcp_subj(subj,4)
+print(subj.subj)    
+
+
+def MultiThr70to99(subj):
+    Lsrf=(subj.Lcoords,subj.Lfaces)
+    Rsrf=(subj.Rcoords,subj.Rfaces)
+
+    thr=optimThresh(subj.subj)
+    print(f'optimal threshold for {subj.subj} is {thr}')
+    
+    dL=[]
+    dR=[]
+    for thr in range(70,99):
+      L10,R10=subj.extract_topX(subj.Lgrad,subj.Rgrad,thr)
+      dL.append(surfdist.analysis.dist_calc(Lsrf,subj.Lfill,L10))
+      dR.append(surfdist.analysis.dist_calc(Rsrf,subj.Rfill,R10))
+    return np.vstack(dL),np.vstack(dR)
+    
+Ltop,Rtop=TopToCort(subj)
+np.save(f'{out}/L.70to99Thr',Ltop)
+np.save(f'{out}/R.70to99Thr',Rtop)
