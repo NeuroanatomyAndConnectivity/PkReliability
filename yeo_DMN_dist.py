9ca4b10b50c11c48388cb71386ec82742cfe8ded
Austin Benn <r.austinbenn@gmail.com>
2023-02-09 14:39:16 +0100

    add group gradient measures

--- yeo_DMN_dist.py
+++ yeo_DMN_dist.py
@@ -23,11 +23,22 @@ Rsrf=(subj.Rcoords,subj.Rfaces)
 Lyeo=np.load('./32k_fs_LR/yeo/yeo.L.DMN.npy')
 Lyeo=np.where(Lyeo==1)[0]
 Lyeo=surfdist.analysis.dist_calc(Lsrf,subj.Lfill,Lyeo)
-np.save(f'{out}/L.yeo',Lyeo)
+np.save(f'{out}/L.yeo.npy',Lyeo)
+
+
+Lgrp=np.load('./32k_fs_LR/top90GrpGrad.L.32k.npy')
+Lgrp=np.where(Lgrp==1)[0]
+Lgrp=surfdist.analysis.dist_calc(Lsrf,subj.Lfill,Lgrp)
+np.save(f'{out}/L.grpGrad90.npy',Lgrp)
 
 
 Ryeo=np.load('./32k_fs_LR/yeo/yeo.R.DMN.npy')
 Ryeo=np.where(Ryeo==1)[0]
 Ryeo=surfdist.analysis.dist_calc(Rsrf,subj.Rfill,Ryeo)
-np.save(f'{out}/R.yeo',Ryeo)
+np.save(f'{out}/R.yeo.npy',Ryeo)
 
+
+Rgrp=np.load('./32k_fs_LR/top90GrpGrad.R.32k.npy')
+Rgrp=np.where(Rgrp==1)[0]
+Rgrp=surfdist.analysis.dist_calc(Rsrf,subj.Rfill,Rgrp)
+np.save(f'{out}/R.grpGrad90.npy',Rgrp)
